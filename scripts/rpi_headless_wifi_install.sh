5a8371cdc5308a9dd0b9f5bd8ce4a092317ae2f3
hpaos <120318998+hpaos@users.noreply.github.com>
2025-01-01 21:37:02 +0100

    Update rpi_headless_wifi_install.sh

--- scripts/rpi_headless_wifi_install.sh
+++ scripts/rpi_headless_wifi_install.sh
@@ -83,7 +83,7 @@ if [ "$VERSION" == "12 (bookworm)" ]; then
 fi;
 # Installing pip3 and venv..  Raspberry lite does not have them
 echo "Installing python3-pip ... pip3 required"
-apt-get install -y python3-pip python3-dotenv
+apt-get install -y python3-pip
 rm /etc/pip.conf
 echo "Installing python3-venv ... vend required" 
 apt-get install -y python3-venv
